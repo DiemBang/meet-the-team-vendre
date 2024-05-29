4707653fda74af6760bed31fdb47fd60da41170b
Diem Bang <diembang09@gmail.com>
2024-05-29 22:54:18 +0200

    Add portrait component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Portrait from './components/Portrait.vue'
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Our Team" />
+  <Portrait name="Diem" contact="diem@mail.com"/>
 </template>
 
 <style scoped>
---
+++ src/components/Portrait.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  name: String,
+  contact: String
+})
+
+// const count = ref(0)
+</script>
+
+<template>
+    <section class="portrait">
+        <div class="name">{{ name }}</div>
+        <div class="contact"><a :href="`mailto:${contact}`">Contact me</a></div>
+    </section>
+</template>
