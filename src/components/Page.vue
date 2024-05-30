4b84c5b0d141d54fc46fa7ce484b8d5b5617f875
Diem Bang <diembang09@gmail.com>
2024-05-30 22:07:03 +0200

    Add click events and functions for next and previous page buttons

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -6,7 +6,7 @@ import axios from "axios";
 
 const apiUrl = "https://reqres.in/api/users";
 const users = ref([]);
-let currentPage = 1;
+let currentPage = ref(1);
 
 const getUsers = async (page) => {
     let url = apiUrl +"?page="+page;
@@ -18,7 +18,13 @@ const getUsers = async (page) => {
 getUsers(currentPage);
 
 const goToNextPage = () => {
-    getUsers(2);
+    currentPage.value = 2;
+    getUsers(currentPage);    
+}
+
+const goToPreviousPage = () => {
+    currentPage.value = 1;
+    getUsers(currentPage);    
 }
 </script>
 
@@ -29,5 +35,12 @@ const goToNextPage = () => {
             <Portrait :firstName="user.first_name" :lastName="user.last_name" :contact="user.contact" :img="user.avatar" />
         </div>
     </div>
-    <button id="nextPage" @click="goToNextPage()">Next page</button>
+    <button id="nextPage" @click="goToNextPage()" :class="{hidden:currentPage===2}">Next page</button>
+    <button id="previousPage" @click="goToPreviousPage()" :class="{hidden:currentPage===1}">Previous page</button>
 </template>
+
+<style scoped>
+.hidden {
+    display: none;
+}
+</style>
