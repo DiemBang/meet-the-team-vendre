12a25162a2e0fb4845565b564aac0992873535e6
Diem Bang <diembang09@gmail.com>
2024-05-31 14:51:34 +0200

    Fix previous and next page buttons

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -7,19 +7,21 @@ import axios from "axios";
 const apiUrl = "https://reqres.in/api/users";
 const users = ref([]);
 let currentPage = ref(1);
+const totalPages = ref(0);
 
 const getUsers = async (page) => {
     let url = apiUrl +"?page="+page;
   await axios.get(url).then((response) => {
+    console.log(response);
     users.value = response.data.data
-    console.log(response.data)
+    totalPages.value = response.data.total_pages
   })
 }
 getUsers(currentPage.value);
 
 const goToNextPage = () => {
     currentPage.value = 2;
-    getUsers(currentPage.value);    
+    getUsers(currentPage.value);
 }
 
 const goToPreviousPage = () => {
@@ -29,18 +31,31 @@ const goToPreviousPage = () => {
 </script>
 
 <template>  
-    <h1>Our Team</h1>
-    <div class="portraits">
-        <div class="portrait-container" v-for="user in users">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <div class="page-background">
+        <h1>Our Team</h1>
+    
+        <div class="portraits">
+            <div class="portrait-container" v-for="user in users">
             <Portrait :firstName="user.first_name" :lastName="user.last_name" :contact="user.contact" :img="user.avatar" />
+            </div>
         </div>
+        <button id="nextPage" @click="goToNextPage()" :class="{hidden:currentPage===totalPages}">Next page</button>
+        <button id="previousPage" @click="goToPreviousPage()" :class="{hidden:currentPage===1}">Previous page</button>
     </div>
-    <button id="nextPage" @click="goToNextPage()" :class="{hidden:currentPage===2}">Next page</button>
-    <button id="previousPage" @click="goToPreviousPage()" :class="{hidden:currentPage===1}">Previous page</button>
 </template>
 
 <style scoped>
+
+.page-background {
+    width: 100%;
+}
+
 .hidden {
     display: none;
 }
+</style>
+
+<style>
+
 </style>
